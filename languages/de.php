f3568d517c7d72dd09452db663e6b57e54967126
Christoph M. Becker <cmbecker69@gmx.de>
2025-05-24 17:12:16 +0200

    Support doughnut charts

--- languages/de.php
+++ languages/de.php
@@ -19,6 +19,7 @@ $plugin_tx['chart']['label_save']="Speichern";
 $plugin_tx['chart']['label_transposed']="transponiert";
 $plugin_tx['chart']['label_type']="Typ";
 $plugin_tx['chart']['label_type_bar']="Säule";
+$plugin_tx['chart']['label_type_doughnut']="Ring";
 $plugin_tx['chart']['label_type_line']="Linie";
 $plugin_tx['chart']['label_type_pie']="Kreis";
 $plugin_tx['chart']['label_values']="Werte";
--- languages/en.php
+++ languages/en.php
@@ -19,6 +19,7 @@ $plugin_tx['chart']['label_save']="Save";
 $plugin_tx['chart']['label_transposed']="transposed";
 $plugin_tx['chart']['label_type']="Type";
 $plugin_tx['chart']['label_type_bar']="Bar";
+$plugin_tx['chart']['label_type_doughnut']="Doughnut";
 $plugin_tx['chart']['label_type_line']="Line";
 $plugin_tx['chart']['label_type_pie']="Pie";
 $plugin_tx['chart']['label_values']="Values";
