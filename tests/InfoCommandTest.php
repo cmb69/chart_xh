cafb29764008fa427b5bb312ace34804e9f90098
Christoph M. Becker <cmbecker69@gmx.de>
2025-05-26 14:21:37 +0200

    Prepare release

--- tests/InfoCommandTest.php
+++ tests/InfoCommandTest.php
@@ -31,7 +31,7 @@ class InfoCommandTest extends TestCase
     public function testRendersPluginInfo(): void
     {
         $response = $this->sut()();
-        $this->assertSame("Chart 1.0-dev", $response->title());
+        $this->assertSame("Chart 1.0", $response->title());
         Approvals::verifyHtml($response->output());
     }
 }
